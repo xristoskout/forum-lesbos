fb6943a3b2910c38a0402dfa45c810908d0bb612
xristoskout <31740784+xristoskout@users.noreply.github.com>
2025-06-04 12:24:39 +0300

    Create widget.js

---
+++ widget.js
@@ -0,0 +1,7 @@
+// widget.js – φορτώνει το chatbot
+(function() {
+  const script = document.createElement("script");
+  script.src = "https://xristoskout.github.io/forum-lesbos/booky-chat-ui.js";
+  script.defer = true;
+  document.head.appendChild(script);
+})();
